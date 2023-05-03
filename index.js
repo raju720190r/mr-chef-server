0c46f06f0c02e89e72c3b70579ffd968a9a33f87
raju720190r <raju720190r@gmail.com>
2023-05-04 00:09:56 +0600

    Single chef Data Loading

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require('cors')
 const port =process.env.PORT || 5050;
 
 const  cData= require('./Data/cData.json');
+const req = require("express/lib/request");
+const res = require("express/lib/response");
 
 app.use(cors());
 
@@ -11,8 +13,16 @@ app.get('/',(req,res)=>{
     res.send('Chef is Running')
 });
 
+
+
 app.get('/cData',(req,res)=>{
     res.send(cData)
+});
+
+app.get('/cData/:id',(req,res)=>{
+  const id=req.params.id;
+  const chefDetails =cData.find(n=>n.id === id)
+  res.send(chefDetails)
 })
 
 app.listen(port,()=>{
