1779dbb55edac1e16348a7647557e4809e1f28c3
raju720190r <raju720190r@gmail.com>
2023-05-05 15:56:47 +0600

    Server Key Hide

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
-const express = require("express");
+const express = require('express');
+const cors = require('cors');
 const app = express();
-const cors = require('cors')
-const port =process.env.PORT || 5050;
-
-const  cData= require('./Data/cData.json');
-const req = require("express/lib/request");
-const res = require("express/lib/response");
-
 app.use(cors());
-
-app.get('/',(req,res)=>{
-    res.send('Chef is Running')
+const port = process.env.PORT || 5000;
+const chefs = require('./data/chefs.json');
+const blogs = require('./data/blog.json')
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello Server is Running' });
 });
 
-
-
-app.get('/cData',(req,res)=>{
-    res.send(cData)
+app.get('/chefs', (req, res) => {
+    res.send(chefs);
 });
-
-app.get('/cData/:id',(req,res)=>{
-  const id=req.params.id;
-  const chefDetails =cData.find(n=>n.id === id)
-  res.send(chefDetails)
+app.get('/chef/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedData = chefs.find(n => n.id == id);
+    res.send(selectedData);
+});
+app.get('/blogs',(req,res)=>{
+    res.send(blogs)
 })
 
-app.listen(port,()=>{
-    console.log(`Chef Server is Running Now Port:${port}`)
+app.listen(port, () => {
+    console.log(`Server is runnig port: ${port}`);
 });
